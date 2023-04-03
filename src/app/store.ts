95a1747711b3f7bc5ec12b88f91595be97ba0b40
Sudhanshu Tripathi <sudhanshutripathi008@gmail.com>
2023-04-04 01:41:00 +0530

    added RTK Query

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/CounterSlice";
+import { dogsApiSlice } from "../features/dogs/DogsApiSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [dogsApiSlice.reducerPath]: dogsApiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(dogsApiSlice.middleware);
   },
 });
 
